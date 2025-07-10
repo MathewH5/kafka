d6745439681cd27067930cc59594c996a118f076
Mathew <matheoshanna10@gmail.com>
2025-07-10 18:56:45 -0300

    first commit

---
+++ src/be/com/mathew/comandos/InicalizacaoKafka.java
@@ -0,0 +1,11 @@
+package be.com.mathew.comandos;
+
+// ir ate o local do arquivo
+// cd(local)
+// cd C:\compactados\kafka_2.13-3.5.0
+
+// inicializar o zookeeper
+//  .\bin\zookeeper-security-migration.sh .\config\zookeeper.properties
+
+// inicializar o kafka
+// .\bin\windows\kafka-server-start.bat .\config\server.properties
